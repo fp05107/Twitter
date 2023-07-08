e8e89a0cec9d70e06115eab20c4a7f782e9cd668
subham <andharalauri@gmail.com>
2023-07-08 12:04:11 +0530

    tweet created

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -17,14 +17,14 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
+    // SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    // ...FontAwesome.font,
   });
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
+  // useEffect(() => {
+  //   if (error) throw error;
+  // }, [error]);
 
   return (
     <>
---
+++ components/Tweet.tsx
@@ -0,0 +1,85 @@
+import { StyleSheet, Image, Text, View } from "react-native";
+import { TweetType } from "../types";
+import { Entypo, EvilIcons } from "@expo/vector-icons";
+import IconButton from "./IconButton";
+
+
+type TweetProps = {
+  tweet: TweetType;
+};
+
+const Tweet = ({ tweet }: TweetProps) => {
+  return (
+    <View style={styles.container}>
+      <Image source={tweet.user.image} style={styles.userImage} />
+      <View style={styles.mainContainer}>
+        <View style={{ flexDirection: "row" }}>
+          <Text style={styles.name}>{tweet.user.name}</Text>
+          <Text style={styles.username}>{tweet.user.username} · 2h</Text>
+          <Entypo
+            name="dots-three-horizontal"
+            size={16}
+            color="gray"
+            style={{ marginLeft: "auto" }}
+          />
+        </View>
+        <Text style={styles.content}>{tweet.content}</Text>
+        {tweet.image && <Image source={tweet.image} style={styles.image} />}
+
+        <View style={styles.footer}>
+          <IconButton icon="comment" text={tweet.numberOfComments} />
+          <IconButton icon="retweet" text={tweet.numberOfRetweets} />
+          <IconButton icon="heart" text={tweet.numberOfLikes} />
+          <IconButton icon="chart" text={tweet.impressions || 0} />
+          <IconButton icon="share-apple" />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Tweet;
+
+const styles = StyleSheet.create({
+  container: {
+    // alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    padding: 10,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderColor: "lightgrey",
+  },
+  userImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+  },
+  mainContainer: {
+    marginLeft: 10,
+    flex: 1,
+  },
+  name: {
+    fontWeight: "600",
+    color: "#00000",
+  },
+  username: {
+    color: "grey",
+    margin: 5,
+  },
+  content: {
+    lineHeight: 20,
+    marginTop: 5,
+    color: "#00000",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 16 / 9,
+    marginVertical: 12,
+    borderRadius: 15,
+  },
+  footer: {
+    flexDirection: "row",
+    marginVertical: 5,
+    justifyContent: "space-between",
+  },
+});
